3f990a34ad1b7b92be2f5291854ce7ca333cebf0
Hammad Mehmood <hammad.mehmood898@gmail.com>
2024-04-02 15:31:42 +0500

    Add project files.

---
+++ Server/Controllers/ClientsControllers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using MidExam.Shared.Models;
+
+namespace MidExam.Server.Controllers
+{
+    public class ClientsControllers : Controller
+    {
+        private readonly IDataAccessProvider _dataAccessProvider;
+        public ClientsControllers(IDataAccessProvider dataAccessProvider)
+        {
+            _dataAccessProvider = dataAccessProvider;
+        }
+
+        [HttpGet]
+        [Route("api/Clients/Get")]
+        public IEnumerable<Persons> Get()
+        {
+            return _dataAccessProvider.GetClientRecord();
+        }
+        [HttpPost]
+        [Route("api/clients/Create")]
+        public void Create([FromBody] Persons person)
+        {
+            if(ModelState.IsValid)
+            {
+                _dataAccessProvider.AddClientRecord(person);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/Clients/Details/{id}")]
+        public Persons Details(int id) 
+        { 
+            return _dataAccessProvider.GetClientRecord(id);
+        }
+
+        [HttpPut]
+        [Route("api/Clients/Edit")]
+        public void Edit([FromBody] Persons person)
+        {
+            if(ModelState.IsValid)
+            {
+                _dataAccessProvider.UpdateClientRecord(person);
+            }
+        }
+        [HttpDelete]
+        [Route("api/Clients/Delete/{id}")]
+        public void DeleteConfirmed(int id)
+        {
+            _dataAccessProvider.DeleteClientRecord(id);
+        }
+    }
+}
---
+++ Server/DataAccess/DomainModelSqlContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MidExam.Shared.Models;
+
+namespace MidExam.Server.DataAccess
+{
+    public class DomainModelSqlContext : DbContext
+    {
+        public DomainModelSqlContext(DbContextOptions<DomainModelSqlContext> options):base(options) 
+        { }
+        public DbSet<Persons> Info { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+
+    }
+}
